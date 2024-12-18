cb183e483c109608f8c83549cafa3e25e2354a5d
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-12-18 21:11:52 +0530

    Add files via upload

---
+++ OOPS/lab_programs/10_pro/MainClass.java
@@ -0,0 +1,8 @@
+import mypack.MyPackageClass; // Import the mypack package
+
+public class MainClass {
+    public static void main(String[] args) {
+        MyPackageClass obj = new MyPackageClass();
+        obj.display(); // Call the display method
+    }
+}
---
+++ OOPS/lab_programs/10_pro/mypack/MyPackageClass.java
@@ -0,0 +1,7 @@
+package mypack; // Package declaration
+
+public class MyPackageClass {
+    public void display() {
+        System.out.println("Hello from MyPackageClass in mypack package!");
+    }
+}
